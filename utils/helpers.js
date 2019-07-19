3a475f2f885f8c757f897cec3f9f8e45d4e6fbb5
Pranav Acharya <pranav.acharya@sap.com>
2019-07-19 14:53:00 +0530

    fix avatar text for blank text input

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,6 +3,7 @@ export const emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
 export const phoneNumberRegex = /[2-9]{2}\d{8}/;
 
 export const getAvatarText = (text) => {
+  if (!text) { return null; }
   const splits = text.split(' ');
   let avatarText = splits[0][0];
   if (splits.length > 1) { avatarText += splits[1][0]; } else { avatarText += splits[0][1]; }
