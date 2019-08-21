16eae45ed10b99f460a0ac01a48891dae6560dde
Pranav Acharya <pranav.acharya@sap.com>
2019-08-21 22:45:27 +0530

    updates welcome component

--- components/WelcomeComponent.js
+++ components/WelcomeComponent.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
+import { formButtonStyle } from '../utils/styles';
 
-const logo = require('../assets/snack-icon.png');
+// const logo = require('../assets/snack-icon.png');
 
 class WelcomeComponent extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Image source={logo} />
-        <Text>This is a welcome testingscreen</Text>
+        {/* <Image source={logo} /> */}
+
+        <Text style={{ fontSize: 22 }}>
+          <Text style={{ fontWeight: 'bold' }}>Urban</Text>
+          <Text>Trainer</Text>
+        </Text>
+
+        <Button title="Get Started" style={formButtonStyle} />
       </View>
     );
   }
